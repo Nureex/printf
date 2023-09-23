4e6df39194ebb3e67e9b04f3a6476cf5ed5a2c53
nureex <nureexamahmud@gmail.com>
2023-09-23 09:36:40 -0700

    Get Size

---
+++ extract_format_size.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * extract_format_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int extract_format_size(const char *format, int *i)
+{
+	int curr_i = *j + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*j = curr_i - 1;
+	else
+		*j = curr_i;
+
+	return (size);
+}
