85be10804c9c32bf98372c42ef386f831b9dbcae
Aaliiyuu <110990492+Aaliiyuu@users.noreply.github.com>
2023-09-23 20:21:16 +0100

    Extract Format Precision

---
+++ extract_format_precision.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * extract_format_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int extract_format_precision(const char *format, int *j, va_list list)
+{
+	int curr_i = *j + 1;
+	int precision = -1;
+
+	if (format[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*j = curr_i - 1;
+
+	return (precision);
+}
